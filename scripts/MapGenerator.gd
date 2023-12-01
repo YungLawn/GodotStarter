d976243e42a1cd4e00ee4ffd6adf6c457ef03814
YungLawn <ccteelon@gmail.com>
2023-12-01 16:28:21 -0500

    procgen

--- scripts/MapGenerator.gd
+++ scripts/MapGenerator.gd
@@ -1,7 +1,7 @@
 @tool
 extends TileMap
 
-const MAP_SIZE = Vector2(50, 50)
+@export var MAP_SIZE = Vector2(100, 100)
 const LAND_CAP = 0.1
 var noise = FastNoiseLite.new()
 var lastChunk = Vector2(0,0)
@@ -9,14 +9,15 @@ var lastChunk = Vector2(0,0)
 @export var seed = 1
 
 func _ready():
+	print(str(seed))
 	noise.seed = seed
-	generate_chunk(player.position)
+	Vector2(0,0)
+	#generate_chunk(player.position)
 	
 func _process(delta):
 		noise.seed = seed
-		noise.cellular_distance_function = 1
-		if Engine.is_editor_hint():
-			generate_chunk($"../player".position)
+		#if Engine.is_editor_hint():
+			#generate_chunk($"../player".position)
 
 	#$"../player/Debug4".text = str(lastChunk.distance_to(player.position))
 	#if(lastChunk.distance_to(player.position) >= 20):
@@ -38,9 +39,9 @@ func generate_chunk(position):
 			var a = noise.get_noise_2d(tile_pos.x-MAP_SIZE.x/2 + x, tile_pos.y-MAP_SIZE.y/2 + y)
 			if a <= LAND_CAP:
 				land.append(Vector2(tile_pos.x-MAP_SIZE.x/2 + x, tile_pos.y-MAP_SIZE.y/2 + y))
-#				set_cell(0, Vector2(tile_pos.x-MAP_SIZE.x/2 + x, tile_pos.y-MAP_SIZE.y/2 + y),0 , Vector2(2, 1), 0)
+				#set_cell(0, Vector2(tile_pos.x-MAP_SIZE.x/2 + x, tile_pos.y-MAP_SIZE.y/2 + y),0 , Vector2(2, 1), 0)
 			elif a >= LAND_CAP:
-#				water.append(Vector2(tile_pos.x-MAP_SIZE.x/2 + x, tile_pos.y-MAP_SIZE.y/2 + y))
+				#water.append(Vector2(tile_pos.x-MAP_SIZE.x/2 + x, tile_pos.y-MAP_SIZE.y/2 + y))
 				set_cell(0, Vector2(tile_pos.x-MAP_SIZE.x/2 + x, tile_pos.y-MAP_SIZE.y/2 + y),0 , Vector2(0, 1), 0)
 	set_cells_terrain_connect(0, land, 0, 0)
 	set_cells_terrain_connect(0, water, 0, 1)
---
+++ scripts/manager.gd
@@ -0,0 +1,9 @@
+@tool
+extends EditorScript
+
+# Called when the script is executed (using File -> Run in Script Editor).
+func _run():
+	var player = get_scene().get_child(1)
+	get_scene().get_child(0).clear()
+	get_scene().get_child(0).generate_chunk(player.position)
+
